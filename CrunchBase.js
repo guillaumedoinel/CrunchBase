9307fab981ead9f916a07b867bdee7eebcbee1f3
guillaumedoinel <guillaumedoinel@yahoo.fr>
2019-06-11 14:31:37 +0200

    Create CrunchBase.js

---
+++ CrunchBase.js
@@ -0,0 +1,57 @@
+(function() {
+  var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function(schemaCallback) {
+    var cols = [{
+      id: "uuid",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "type",
+      alias: "Company Type",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "name",
+      alias: "Company Name",
+      dataType: tableau.dataTypeEnum.string
+    }];
+
+    var tableSchema = {
+      id: "CrunchBaseFeed_Organizations",
+      alias: "List of Organizations",
+      columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+  };
+
+  myConnector.getData = function(table, doneCallback) {
+    $.getJSON("https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3", function(resp) {
+
+      // &categories=Automotive&name=BMW
+
+      var itemsJSON = resp.data.items, // data Structure in JSON to read
+        tableData = [];
+
+      // Iterate over the JSON object
+      for (var i = 0, len = itemsJSON.length; i < len; i++) {
+        tableData.push({
+          "uuid": itemsJSON[i].uuid,
+          "type": itemsJSON[i].type,
+          "name": itemsJSON[i].properties.name
+        });
+      }
+
+      table.appendRows(tableData);
+      doneCallback();
+    });
+  };
+
+  tableau.registerConnector(myConnector);
+
+  $(document).ready(function() {
+    $("#submitButton").click(function() {
+      tableau.connectionName = "CrunchBase Feed";
+      tableau.submit();
+    });
+  });
+})();
