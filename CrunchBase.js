d826f06802a4c3b1ebcb7b0cec6e0d82a1a4d748
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-06-11 17:37:26 +0200

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -43,36 +43,48 @@
       table.appendRows(tableData);
       doneCallback();
     });
-  }; */
+  };*/
 
   // MULTIPLE CALLS API EXAMPLE
   var PageNo = 1;
   myConnector.getData = function(table, doneCallback) {
     do {
-      $.ajax({
+      /*$.ajax({
         url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=" + PageNo,
         async: false,
         sucess: function(response) {  // response is a custom name
           var tableData = [];
-          //var itemsJSON = response.data.items;
-          //for (var i = 0, len = itemsJSON.length; i < len; i++) {
-          /*for (var i in response) {
+          var itemsJSON = response.data.items;
+          for (var i = 0, len = itemsJSON.length; i < len; i++) {
             tableData.push({   // data.items is the CrunchBase API JSON Structure
               "uuid": itemsJSON[i].uuid,
               "type": itemsJSON[i].type,
               "name": itemsJSON[i].properties.name
             });
-          }*/
-
-          tableData.push({   // data.items is the CrunchBase API JSON Structure
-            "uuid": 1,
-            "type": 2,
-            "name": 3
-          });
+          }
 
           table.appendRows(tableData); // append data for each API call
         }
+      });*/
+
+      $.getJSON("https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page="+PageNo, function(resp) {
+
+        // &categories=Automotive&name=BMW
+        var itemsJSON = resp.data.items, // data Structure in JSON to read
+          tableData = [];
+        // Iterate over the JSON object
+        for (var i = 0, len = itemsJSON.length; i < len; i++) {
+          tableData.push({
+            "uuid": itemsJSON[i].uuid,
+            "type": itemsJSON[i].type,
+            "name": itemsJSON[i].properties.name
+          });
+        }
+
+        table.appendRows(tableData);
       });
+
+
       PageNo++;
     } while (PageNo<4);
 
