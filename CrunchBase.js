f9cc1799d4707e33ac7f9922b44c9f40228840ed
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-06-13 17:06:14 +0200

    fd

--- CrunchBase.js
+++ CrunchBase.js
@@ -3,13 +3,9 @@
 
   myConnector.getSchema = function(schemaCallback) {
 
-    // CrunchBase Organizations
     var Organizations_cols = [{
       id: "uuid",
-      dataType: tableau.dataTypeEnum.string
-    }, {
-      id: "type",
-      alias: "Company Type",
+      alias: "Company UUID",
       dataType: tableau.dataTypeEnum.string
     }, {
       id: "name",
@@ -18,9 +14,12 @@
     }];
     var Organizations_Schema = {
       id: "CrunchBase_Organizations",   // table.tableInfo.id
-      alias: "List of Organizations",
+      alias: "Organizations",
       columns: Organizations_cols
     };
+
+
+
 
     schemaCallback([Organizations_Schema]);
   };
@@ -29,53 +28,42 @@
   // As a result, you need a way to change the call to the API for each table. The easiest way to do this is to use the table.tableInfo.id value that we set in the table schemas.
   myConnector.getData = function(table, doneCallback) {
 
+    // ORGANIZATIONS API
     if (table.tableInfo.id == "CrunchBase_Organizations") {
-      // Organizations : MULTIPLE CALLS API EXAMPLE
-      var PageNo = 1;
-      var next_page_url = "init";
-      do {
-        $.ajax({
-          url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=" + PageNo,
-          async: false,
-          success: function(response) {  // response is a custom name
-            var tableData = [];
-            var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
-            for (var i = 0, len = itemsJSON.length; i < len; i++) {
-              tableData.push({
-                "uuid": itemsJSON[i].uuid,
-                "type": itemsJSON[i].type,
-                "name": itemsJSON[i].properties.name
-              });
+
+      // Calls the Organizations API for as many categories listed
+      var CategoriesList = ["Auto Insurance","Automotive","Autonomous Vehicles","Battery","Car Sharing","Electric Vehicle","Last Mile Transportation","Limousine Service","Ride Sharing","Taxi Service"];
+
+      for (var j = 0, len2 = CategoriesList.length; j < len2; j++) {
+        var CategoryName = CategoriesList[j];
+
+        // Organizations : MULTIPLE CALLS to a PAGED API
+        var PageNo = 1;
+        var Next_page_url = "init";
+        do {
+          $.ajax({
+            url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&categories=" + CategoryName + "&page=" + PageNo,
+            async: false,
+            success: function(response) { // response is a custom name
+              var tableData = [];
+              var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
+              for (var i = 0, len = itemsJSON.length; i < len; i++) {
+                tableData.push({
+                  "uuid": itemsJSON[i].uuid,
+                  "name": itemsJSON[i].properties.name
+                });
+              }
+
+              table.appendRows(tableData); // append data for each API call
+              Next_page_url = response.data.paging.next_page_url;
             }
-
-            table.appendRows(tableData); // append data for each API call
-            next_page_url = response.data.paging.next_page_url;
-          }
-        });
-
-        PageNo++;
-      } while (next_page_url != null);    // while there are some data left
-      doneCallback();
-    }
-
-    if (table.tableInfo.id == "CrunchBase_People") {
-      // ONE CALL API EXAMPLE
-      $.getJSON("https://api.crunchbase.com/v3.1/people?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=1", function(resp) {
-
-        var itemsJSON = resp.data.items, // data Structure in JSON to read
-          tableData = [];
-        // Iterate over the JSON object
-        for (var i = 0, len = itemsJSON.length; i < len; i++) {
-          tableData.push({
-            "uuid": itemsJSON[i].uuid,
-            "title": itemsJSON[i].properties.title,
-            "last_name": itemsJSON[i].properties.last_name
           });
-        }
+          PageNo++;
+        } while (Next_page_url != null); // while there are some data left
+      }
 
-        table.appendRows(tableData);
-        doneCallback();
-      });
+
+      doneCallback();
     }
 
   };
--- SandBox.js
+++ SandBox.js
@@ -19,19 +19,38 @@
       columns: Categories_cols
     };*/
 
+    "transaction_type": "Investment",
+    "company_name": Company_Name,
+    "transaction_date": Announced_On,
+    "funded_company_name": FundedCompanyName,
+    "transaction_amount": TransactionAmount
+
+
     // CrunchBase Organizations
     var Organizations_cols = [{
-      id: "uuid",
+      id: "transaction_type",
       alias: "Company UUID",
       dataType: tableau.dataTypeEnum.string
     }, {
-      id: "name",
+      id: "company_name",
       alias: "Company Name",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "transaction_date",
+      alias: "Transaction Date",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "funded_company_name",
+      alias: "Funded Company Name",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "transaction_amount",
+      alias: "Transaction Amount",
       dataType: tableau.dataTypeEnum.string
     }];
     var Organizations_Schema = {
       id: "CrunchBase_Organizations",   // table.tableInfo.id
-      alias: "List of Organizations",
+      alias: "Organizations",
       columns: Organizations_cols
     };
 
@@ -44,13 +63,15 @@
   // As a result, you need a way to change the call to the API for each table. The easiest way to do this is to use the table.tableInfo.id value that we set in the table schemas.
   myConnector.getData = function(table, doneCallback) {
 
+    // ORGANIZATIONS & ORAGANIZATIONS PERMALINK API
     if (table.tableInfo.id == "CrunchBase_Organizations") {
 
       // Calls the Organizations API for as many categories listed
-      var CategoriesList = ["Auto Insurance","Automotive","Autonomous Vehicles","Battery","Car Sharing","Electric Vehicle","Last Mile Transportation","Limousine Service","Ride Sharing","Taxi Service"]; 
+      //var CategoriesList = ["Auto Insurance","Automotive","Autonomous Vehicles","Battery","Car Sharing","Electric Vehicle","Last Mile Transportation","Limousine Service","Ride Sharing","Taxi Service"];
+      var CategoriesList = ["Auto Insurance"];
 
-      for (var j = 0, len2 = CategoriesList.length; j < len2; j++) {
-        var CategoryName = CategoriesList[j];
+      for (var i = 0, len = CategoriesList.length; i < len; i++) {
+        var CategoryName = CategoriesList[i];
 
         // Organizations : MULTIPLE CALLS to a PAGED API
         var PageNo = 1;
@@ -60,16 +81,40 @@
             url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&categories=" + CategoryName + "&page=" + PageNo,
             async: false,
             success: function(response) { // response is a custom name
-              var tableData = [];
-              var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
-              for (var i = 0, len = itemsJSON.length; i < len; i++) {
-                tableData.push({
-                  "uuid": itemsJSON[i].uuid,
-                  "name": itemsJSON[i].properties.name
-                });
-              }
+              //var tableData = [];
+              var organizationsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
+              for (var iO = 0, leniO = organizationsJSON.length; iO < leniO; iO++) { // For each organization, call the organization permalink API to get investments details
+                  var UUID = organizationsJSON[iO].uuid;
+                  var CompanyName = organizationsJSON[iO].properties.name;
+                  var URL = "https://api.crunchbase.com/v3.1/organizations/" + UUID + "?user_key=9df45b533650fb1b95e83357b5da2db3";
 
-              table.appendRows(tableData); // append data for each API call
+                  $.ajax({
+                    url: URL,
+                    async: false,
+                    success: function(response2) {
+                      var investmentTableData = [];
+                      // FIRST GET INVESTMENTS DATA
+                      var investmentsJSON = response2.data.relationships.investments.items;
+                      for (var iI = 0, leniI = investmentsJSON.length; iI < leniI; iI++) {
+                        var Announced_On = investmentsJSON[iI].properties.announced_on;
+                        var FundedCompanyName = investmentsJSON[iI].properties.relationships.invested_in.properties.name;
+                        var TransactionAmount = investmentsJSON[iI].properties.relationships.invested_in.properties.total_funding_usd;
+                        tableData.push({
+                          "transaction_type": "Investment",
+                          "company_name": Company_Name,
+                          "transaction_date": Announced_On,
+                          "funded_company_name": FundedCompanyName,
+                          "transaction_amount": TransactionAmount
+                        });
+                      }
+                      table.appendRows(investmentTableData); // append INVESTMENTS data
+
+                      // THEN GET ACQUISITIONS DATA
+
+                    }
+                  });
+
+              }
               Next_page_url = response.data.paging.next_page_url;
             }
           });
@@ -80,6 +125,31 @@
 
       doneCallback();
     }
+
+    /*// Organizations : MULTIPLE CALLS to a PAGED API
+    var PageNo = 1;
+    var Next_page_url = "init";
+    do {
+      $.ajax({
+        url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&categories=" + CategoryName + "&page=" + PageNo,
+        async: false,
+        success: function(response) { // response is a custom name
+          var tableData = [];
+          var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
+          for (var i = 0, len = itemsJSON.length; i < len; i++) {
+            tableData.push({
+              "uuid": itemsJSON[i].uuid,
+              "name": itemsJSON[i].properties.name
+            });
+          }
+
+          table.appendRows(tableData); // append data for each API call
+          Next_page_url = response.data.paging.next_page_url;
+        }
+      });
+      PageNo++;
+    } while (Next_page_url != null); // while there are some data left*/
+
 
     /*if (table.tableInfo.id == "CrunchBase_People") {
       // ONE CALL API EXAMPLE
