dd8afa334cd5e1f5b8885c24475f9f4aca596e89
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-06-13 10:47:04 +0200

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -2,7 +2,9 @@
   var myConnector = tableau.makeConnector();
 
   myConnector.getSchema = function(schemaCallback) {
-    var cols = [{
+
+    // CrunchBase Organizations
+    var Organizations_cols = [{
       id: "uuid",
       dataType: tableau.dataTypeEnum.string
     }, {
@@ -14,63 +16,85 @@
       alias: "Company Name",
       dataType: tableau.dataTypeEnum.string
     }];
-
-    var tableSchema = {
-      id: "CrunchBaseFeed_Organizations",
+    var Organizations_Schema = {
+      id: "CrunchBase_Organizations",   // table.tableInfo.id
       alias: "List of Organizations",
-      columns: cols
+      columns: Organizations_cols
     };
 
-    schemaCallback([tableSchema]);
+    // CrunchBase People
+    var People_cols = [{
+      id: "uuid",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "title",
+      alias: "Job Title",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "last_name",
+      alias: "Person's Name",
+      dataType: tableau.dataTypeEnum.string
+    }];
+    var People_Schema = {
+      id: "CrunchBase_People",     // table.tableInfo.id
+      alias: "List of People",
+      columns: People_cols
+    };
+
+    schemaCallback([Organizations_Schema,People_Schema]);
   };
 
-  // ONE CALL API EXAMPLE
-  /*myConnector.getData = function(table, doneCallback) {
-    $.getJSON("https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=1", function(resp) {
-
-      // &categories=Automotive&name=BMW
-      var itemsJSON = resp.data.items, // data Structure in JSON to read
-        tableData = [];
-      // Iterate over the JSON object
-      for (var i = 0, len = itemsJSON.length; i < len; i++) {
-        tableData.push({
-          "uuid": itemsJSON[i].uuid,
-          "type": itemsJSON[i].type,
-          "name": itemsJSON[i].properties.name
-        });
-      }
-
-      table.appendRows(tableData);
-      doneCallback();
-    });
-  };*/
-
-  // MULTIPLE CALLS API EXAMPLE
-  var PageNo = 1;
+  // When you create multiple table schemas, the WDC API calls the getData function once for each schema.
+  // As a result, you need a way to change the call to the API for each table. The easiest way to do this is to use the table.tableInfo.id value that we set in the table schemas.
   myConnector.getData = function(table, doneCallback) {
-    do {
-      $.ajax({
-        url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=" + PageNo,
-        async: false,
-        success: function(response) {  // response is a custom name
-          var tableData = [];
-          var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
-          for (var i = 0, len = itemsJSON.length; i < len; i++) {
-            tableData.push({
-              "uuid": itemsJSON[i].uuid,
-              "type": itemsJSON[i].type,
-              "name": itemsJSON[i].properties.name
-            });
+
+    if (table.tableInfo.id == "CrunchBase_Organizations") {
+      // Organizations : MULTIPLE CALLS API EXAMPLE
+      var PageNo = 1;
+      do {
+        $.ajax({
+          url: "https://api.crunchbase.com/v3.1/organizations?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=" + PageNo,
+          async: false,
+          success: function(response) {  // response is a custom name
+            var tableData = [];
+            var itemsJSON = response.data.items; // data.items is the CrunchBase API JSON Structure
+            for (var i = 0, len = itemsJSON.length; i < len; i++) {
+              tableData.push({
+                "uuid": itemsJSON[i].uuid,
+                "type": itemsJSON[i].type,
+                "name": itemsJSON[i].properties.name
+              });
+            }
+
+            table.appendRows(tableData); // append data for each API call
           }
+        });
 
-          table.appendRows(tableData); // append data for each API call
+        PageNo++;
+      } while (PageNo<=4);    // while next_page_url is not null ...
+      doneCallback();
+    }
+
+    if (table.tableInfo.id == "CrunchBase_People") {
+      // ONE CALL API EXAMPLE
+      $.getJSON("https://api.crunchbase.com/v3.1/people?user_key=9df45b533650fb1b95e83357b5da2db3&items_per_page=250&page=1", function(resp) {
+
+        var itemsJSON = resp.data.items, // data Structure in JSON to read
+          tableData = [];
+        // Iterate over the JSON object
+        for (var i = 0, len = itemsJSON.length; i < len; i++) {
+          tableData.push({
+            "uuid": itemsJSON[i].uuid,
+            "type": itemsJSON[i].type,
+            "name": itemsJSON[i].properties.name
+          });
         }
+
+        table.appendRows(tableData);
+        doneCallback();
       });
+    }
 
-      PageNo++;
-    } while (PageNo<=4);    // while next_page_url is not null ...
-
-    doneCallback();
   };
 
   tableau.registerConnector(myConnector);
