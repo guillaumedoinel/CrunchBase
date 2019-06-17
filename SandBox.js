46e7a4cd8c3113f9d52920d567ca98f2ed99fae5
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-06-17 11:54:36 +0200

    Update SandBox.js

--- SandBox.js
+++ SandBox.js
@@ -40,7 +40,7 @@
 
       //var CategoriesList = ["Auto Insurance","Automotive","Autonomous Vehicles","Battery","Car Sharing","Electric Vehicle","Last Mile Transportation","Limousine Service","Ride Sharing","Taxi Service"];
       //var CompaniesList = ["Audi","BMW", "Fiat","Chrysler","Ford","General Motors","Honda","Hyundai","Jaguar","Land Rover","KIA","Lexus","Mazda","Daimler","Mitsubishi","Nissan","Porsche","PSA","Renault","Seat","Skoda","Tesla","Toyota","Volvo","VW" ];
-      var CompaniesList = ["Jaguar","Land Rover","Kia", "Lexus", "Mazda"];
+      var CompaniesList = ["Daimler", "Mitsubishi", "Nissan", "Porsche","PSA" ];
 
       // Iterate for as many companies as listed above
       for (var i = 0, len = CompaniesList.length; i < len; i++) {
