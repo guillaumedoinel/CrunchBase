22408dd266660549a976da03986a920b9bcaa40f
guillaumedoinel <guillaumedoinel@yahoo.fr>
2019-08-22 16:09:27 +0200

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -268,14 +268,14 @@
   // Inserts HARDCODED subsidiaries into the same table as Investments/Acquisitions
   function insertSubsidiaries(p_table) {
     var SubsidiariesList = [
-      {Sector:"OEM",Group:"Daimler",Company:"Daimler",Investor:"Daimler",TransactionType:"Subsidiary",CompanyNameJOIN:"ShareNow",AnnouncedDate:"01/01/2019"},
-      {Sector:"OEM",Group:"BMW",Company:"BMW",Investor:"BMW",TransactionType:"Subsidiary",CompanyNameJOIN:"ShareNow",AnnouncedDate:"01/01/2019"},
-      {Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"Free2Move",AnnouncedDate:"00/01/1900"},
-      {Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"Free2Move Paris",AnnouncedDate:"00/01/1900"},
-      {Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"eMov",AnnouncedDate:"00/01/1900"},
-      {Sector:"OEM",Group:"VAG",Company:"Volkswagen",Investor:"Volkswagen",TransactionType:"Subsidiary",CompanyNameJOIN:"WeShare",AnnouncedDate:"00/01/1900"},
-      {Sector:"OEM",Group:"BMW",Company:"BMW",Investor:"BMW",TransactionType:"Subsidiary",CompanyNameJOIN:"Hive",AnnouncedDate:"00/01/1900"},
-      {Sector:"OEM",Group:"Daimler",Company:"Daimler",Investor:"Daimler",TransactionType:"Subsidiary",CompanyNameJOIN:"Hive",AnnouncedDate:"00/01/1900"}
+      {Sector:"OEM",Group:"Daimler",Company:"Daimler",Investor:"Daimler",TransactionType:"Subsidiary",CompanyNameJOIN:"ShareNow",AnnouncedDate:"2019-01-01"},
+{Sector:"OEM",Group:"BMW",Company:"BMW",Investor:"BMW",TransactionType:"Subsidiary",CompanyNameJOIN:"ShareNow",AnnouncedDate:"2019-01-01"},
+{Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"Free2Move",AnnouncedDate:""},
+{Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"Free2Move Paris",AnnouncedDate:""},
+{Sector:"OEM",Group:"PSA",Company:"PSA",Investor:"PSA",TransactionType:"Subsidiary",CompanyNameJOIN:"eMov",AnnouncedDate:""},
+{Sector:"OEM",Group:"VAG",Company:"Volkswagen",Investor:"Volkswagen",TransactionType:"Subsidiary",CompanyNameJOIN:"WeShare",AnnouncedDate:""},
+{Sector:"OEM",Group:"BMW",Company:"BMW",Investor:"BMW",TransactionType:"Subsidiary",CompanyNameJOIN:"Hive",AnnouncedDate:""},
+{Sector:"OEM",Group:"Daimler",Company:"Daimler",Investor:"Daimler",TransactionType:"Subsidiary",CompanyNameJOIN:"Hive",AnnouncedDate:""}
     ];
 
     var subsidiariesTableData = [];
