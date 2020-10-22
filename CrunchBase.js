88db8626322b3f6d618ef5552b0c942af9fd882b
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2020-10-22 11:26:26 +0200

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -24,8 +24,10 @@
       //var Next_page_url = "init";
       //do {
         $.ajax({
+          type: "GET",
           url: "https://api.crunchbase.com/api/v4/entities/organizations/" + UUID + "?card_ids=participated_investments&user_key=6677554af8f112f1a065561ee7b49233", //+ PageNo, // browse the list of investments
-          async: true,
+          async: false,
+          cache: false,
           indexValue: {
             paramSector: Sector,
             paramGroup: Group,
