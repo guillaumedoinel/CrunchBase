25296d7d96afd52f3f119fa0f246c6f0b6264c2c
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2020-11-12 11:41:37 +0100

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -64,7 +64,7 @@
       //var Next_page_url = "init";
       //do {
         $.ajax({
-          url: "https://api.crunchbase.com/api/v4/entities/organizations/" + UUID + "?user_key=6677554af8f112f1a065561ee7b49233", //+ PageNo, // browse the list of investments
+          url: "https://api.crunchbase.com/api/v4/entities/organizations/general-motors", //+ PageNo, // browse the list of investments
           async: false,
           //async: true, aucun résultat
           type: "POST",
@@ -73,6 +73,7 @@
           contentType: "application/json; charset=utf-8", // The type of data you're sending
           dataType: 'json', //The type of data that you're expecting back from the server
           data: {
+            user_key:"user_key=6677554af8f112f1a065561ee7b49233",
             card_ids:"participated_investments"
           },
           indexValue: {
