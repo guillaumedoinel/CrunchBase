ce15c8d9e6f61d81d6e4eac19564760c2ff62cab
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2020-11-12 12:23:48 +0100

    Update CrunchBase.js

--- CrunchBase.js
+++ CrunchBase.js
@@ -72,6 +72,13 @@
           headers: {
             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Content-Type':'application/json; charset=utf-8',
+            'Host':'api.crunchbase.com',
+            'Pragma':'no-cache',
+            'Sec-Fetch-Dest':'document',
+            'Sec-Fetch-Mode':'navigate',
+            'Sec-Fetch-Site':'none',
+            'Sec-Fetch-User':''?1',
+            'Upgrade-Insecure-Requests:'1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36',
           },
 
