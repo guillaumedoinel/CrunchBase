0b68a6c86e0fb0d198ff70f813fca6ebe120201a
Guillaume <guillaumedoinel@yahoo.fr>
2020-12-10 10:05:57 +0100

    c

--- CrunchBase.js
+++ CrunchBase.js
@@ -69,18 +69,6 @@
           //async: true, aucun résultat
           crossDomain: true,
           type: "POST",
-          headers: {
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Content-Type':'application/json; charset=utf-8',  // The type of data you're sending
-            'Host':'api.crunchbase.com',
-            'Pragma':'no-cache',
-            'Sec-Fetch-Dest':'document',
-            'Sec-Fetch-Mode':'navigate',
-            'Sec-Fetch-Site':'none',
-            'Sec-Fetch-User':'?1',
-            'Upgrade-Insecure-Requests':'1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
-          },
           dataType: 'json', //The type of data that you're expecting back from the server
           data: {
             user_key:"6677554af8f112f1a065561ee7b49233",
